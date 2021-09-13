cb20dae437847c60f9026b276770257826c0e0ce
Bipul Chaudhary <bipulchau@gmail.com>
2021-09-13 18:28:39 -0400

    Created own assert function

---
+++ assertEqual.js
@@ -0,0 +1,12 @@
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log("✅ Assertion Passed: " + actual + " === " + expected);
+  } else {
+    console.log("❌ Assertion Failed: " + actual + "  !== " + expected);
+
+  }
+};
+
+assertEqual("Lighthouse Lab", "Bootcamp");
+assertEqual(1,1);
+assertEqual("Lighthouse Lab", "Lighthouse Lab");
