3c8f325d6f9a0857b737cea51c76de0b58d96bd5
Bipul Chaudhary <bipulchau@gmail.com>
2021-09-16 18:14:10 -0400

    changed code in assertArraysEqual

--- assertArraysEqual.js
+++ assertArraysEqual.js
@@ -13,15 +13,16 @@ const eqArrays = function(arr1, arr2) {
   } return true;
 };
 
-const assertArraysEqual = function(eqArrays, expected) {
-  if (eqArrays === expected) {
-    console.log (`✅ Assertion Passed: ${eqArrays} === ${expected}`);
+const assertArraysEqual = function(actual, expected) {
+  if (eqArrays(actual, expected)) {
+    console.log (`✅ Assertion Passed: ${actual} === ${expected}`);
   } else {
-    console.log (`❌ Assertion Failed: ${eqArrays} !== ${expected}`);
+    console.log (`❌ Assertion Failed: ${actual} !== ${expected}`);
   }
 };
 
-assertArraysEqual(eqArrays([1,2,3], [1,2,3]), true)
-assertArraysEqual(eqArrays([1,2,3], [2,2,1]), true)
-assertArraysEqual(eqArrays(["1", "2" ,"3"], ["1","2","3"]), true)
-assertArraysEqual(eqArrays(["1", "2" ,"3"], ["1","2",3]), true)
+
+assertArraysEqual([1,2,3], [1,2,3])
+assertArraysEqual([1,2,3], [2,2,1])
+assertArraysEqual(["1", "2" ,"3"], ["1","2","3"])
+assertArraysEqual(["1", "2" ,"3"], ["1","2",3])
