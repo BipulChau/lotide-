07f64156fea67e66cd96a19ddf02b842db5bc2b1
Bipul Chaudhary <bipulchau@gmail.com>
2021-09-15 22:40:54 -0400

    added a loop to check equality if property is an array

--- eqObjects.js
+++ eqObjects.js
@@ -21,34 +21,40 @@ const eqArrays = function(arr1, arr2) {
   } return true;
 };
 
+const eqObjects = function(object1, object2) {
+  if (Object.keys(object1).length !== Object.keys(object2).length) {
+    return false;
+  } else {
+    for (let i of Object.keys(object1)) {
+      if (Array.isArray(object2[i]) && Array.isArray(object1[i])) {
+        if (!eqArrays(object2[i], object1[i])) {
+          return false;
+        } else {
+          if (!Array.isArray(object2[i]) && !Array.isArray(object1[i])) {
+            if ((object2[i]) !== object1[i]) {
+              return false;
+            }
+          }
+        } return true;
+      } else if (object2[i] !== object1[i]) {
+        return false;
+      }
+    }
+  } return true;
+
+};
 // const eqObjects = function(object1, object2) {
 //   if (Object.keys(object1).length !== Object.keys(object2).length){
 //     return false;
 //   } else {
 //     for(let i of Object.keys(object1)){
-//       if (Array.isArray(object2[i]) && Array.isArray(object1[i])){
-//         if (eqArrays(object2[i], object1[i])){
-          
-//         }
-//       } else if (object2[i] !== object1[i]){
+//       if (object2[i] !== object1[i]){
 //         return false
 //       }
 //     }
 //   } return true;
 
-//}
-const eqObjects = function(object1, object2) {
-  if (Object.keys(object1).length !== Object.keys(object2).length){
-    return false;
-  } else {
-    for(let i of Object.keys(object1)){
-      if (object2[i] !== object1[i]){
-        return false
-      } 
-    }
-  } return true;
-
-}
+// }
 
 
 let ab = {a: "1", b: "2"};
@@ -59,12 +65,16 @@ assertEqual(eqObjects(ab,ba), true);
 assertEqual(eqObjects(ab, abc), true);
 assertEqual(eqObjects(abc, def), true);
 
-// let cd = {c: "1", d:["2", 3]};
-// let dc = {d: ["2", 3], c: "1"};
-// assertEqual(eqObjects(cd, dc), true)
+let cd = {c: "1", d:["2", 3]};
+let dc = {d: ["2", 3], c: "1"};
+assertEqual(eqObjects(cd, dc), true);
 
-// let cd2 = {c: "1", d: ["2", 3 ,4]};
-// assertEqual(eqObjects(cd, cd2), true)
+let ef = {c: "1", d:["2", 3]};
+let gh = {d: ["2", 3], c: "2"};
+assertEqual(eqObjects(ef, gh), true);
+
+let cd2 = {c: "1", d: ["2", 3 ,4]};
+assertEqual(eqObjects(cd, cd2), true);
 // //console.log(eqArrays(cd["d"], cd2["d"]));
 
 // console.log(eqObjects(cd, dc))
