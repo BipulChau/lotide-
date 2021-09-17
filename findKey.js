63f5ba20359985ff3555587de2f367e2e92b13fe
Bipul Chaudhary <bipulchau@gmail.com>
2021-09-17 14:19:50 -0400

    changed to obj[key]

--- findKey.js
+++ findKey.js
@@ -7,9 +7,9 @@ const assertEqual = function(actual, expected) {
 };
 
 const findKey = function(obj, callback) {
-  for (let prop in obj) {
-    if (callback(prop)) {
-      return prop;
+  for (let key in obj) {
+    if (callback(obj[key])) {
+      return key;
     }
   } return undefined;
 };
@@ -20,6 +20,9 @@ let bestTVShowsByGenre = {
   drama: "The Wire"
 };
 
-let result = findKey(bestTVShowsByGenre, x => bestTVShowsByGenre[x]==="The Wire");
+let result = findKey(bestTVShowsByGenre, x => x === "The Wire");
 console.log (result)
 
+let result1 = findKey({    "Blue Hill": { stars: 1 },    "Akaleri": { stars: 3 },    "noma": { stars: 2 },    "elBulli": { stars: 3 },    "Ora": { stars: 2 },    "Akelarre": { stars: 3 }  }, x => x.stars === 2)
+console.log(result1)
+assertEqual(findKey({    "Blue Hill": { stars: 1 },    "Akaleri": { stars: 3 },    "noma": { stars: 2 },    "elBulli": { stars: 3 },    "Ora": { stars: 2 },    "Akelarre": { stars: 3 }  }, x => x.stars === 2), "noma")
