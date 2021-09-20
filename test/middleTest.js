06c9c41251f604b513f5fc0c6a8f82f7a56f2c98
Bipul Chaudhary <bipulchau@gmail.com>
2021-09-20 15:54:40 -0400

    Refactored test with Mocha functions and chai assertions

--- test/middleTest.js
+++ test/middleTest.js
@@ -1,20 +1,33 @@
-const assertArraysEqual = require('../assertArraysEqual');
+const assert = require('chai').assert;
 const middle = require('../middle');
 
+describe("middle", () => {
+  it("should return [] for []", () => {
+    assert.deepEqual(middle([]), []);
+  });
+ 
+  it("should return [] for [8]", () => {
+    assert.deepEqual(middle([8]), []);
+  });
 
-console.log(assertArraysEqual(middle([1,2,3,4,5]), [3]));
-console.log(assertArraysEqual(middle([99]), []));
-console.log(assertArraysEqual(middle([1, 2, 3, 4, 5, 6, 7, 8, 9, 10,11,12,13,14]), [7,8]));
+  it("should return [] for [8, 11]", () => {
+    assert.deepEqual(middle([8, 11]), []);
+  });
 
+  it("should return [] for []", () => {
+    assert.deepEqual(middle([]), []);
+  });
 
-console.log(middle([1]));
-console.log(middle([1, 2]));
-console.log(middle([1, 2, 3]));
-console.log(middle([1, 2, 3, 4,5]));
-console.log(middle([1, 2, 3, 4]));
-console.log(middle([1, 2, 3, 4, 5, 6]));
-console.log(middle([1, 2, 3, 4, 5, 6, 7, 8]));
-console.log(middle([1, 2, 3, 4, 5, 6, 7, 8, 9, 10]));
-console.log(middle([1, 2, 3, 4, 5, 6, 7, 8, 9, 10,]));
-console.log(middle([1, 2, 3, 4, 5, 6, 7, 8, 9, 10,11,12]));
-console.log(middle([1, 2, 3, 4, 5, 6, 7, 8, 9, 10,11,12,13,14]));
+  it("should return [9] for [8, 9, 11]", () => {
+    assert.deepEqual(middle([8, 9, 11]), [9]);
+  });
+
+  it("should return [11] for [8, 9, 11, 33, 21]", () => {
+    assert.deepEqual(middle([8, 9, 11, 33, 21]), [11]);
+  });
+
+  it("should return [11] for [8, 9, 11, 33, 21]", () => {
+    assert.deepEqual(middle([1, 2, 3, 4, 5, 6, 7, 8, 9, 10,11,12,13,14]), [7,8]);
+  });
+
+})
